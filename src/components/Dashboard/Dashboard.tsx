7919693647b98fb7b74b93929cbad213d42ebb07
islamBelabbes <belabbesislam2@gmail.com>
2024-02-15 23:39:22 +0100

    add tables to dashboard main view

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -7,6 +7,12 @@ import {
 } from "@/components/ui/card";
 import { Users2Icon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { ProjectsTable } from "./tables/projectsTable/data-table";
+import { CategoriesTable } from "./tables/categoriesTable/data-table";
+import { columns as projectsColumns } from "./tables/projectsTable/columns";
+import { categories, projects } from "@/seed";
+import { columns } from "./tables/categoriesTable/columns";
+import { Badge } from "@/components/ui/badge";
 
 type DashboardStatusProps = {
   Icon: JSX.Element;
@@ -43,6 +49,16 @@ const Dashboard = () => {
           status={200}
           title="Total Users"
         />
+      </div>
+      <div className="mt-4 flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
+          <Badge className="w-fit font-semibold text-sm">last Projects</Badge>
+          <ProjectsTable columns={projectsColumns} data={projects} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Badge className="w-fit font-semibold text-sm">last Categories</Badge>
+          <CategoriesTable columns={columns} data={categories} />
+        </div>
       </div>
     </div>
   );
