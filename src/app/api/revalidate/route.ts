c72f8598d9a18931e7b22211c2538e471fc962fb
islamBelabbes <belabbesislam2@gmail.com>
2024-02-25 17:58:43 +0100

    accept apikey from searchParams

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,7 +2,9 @@ import { revalidatePath } from "next/cache";
 import { NextRequest } from "next/server";
 
 export function POST(req: NextRequest) {
-  const apikey = req.headers.get("apikey");
+  const { searchParams } = new URL(req.url);
+
+  const apikey = req.headers.get("apikey") || searchParams.get("apikey");
 
   if (apikey !== process.env.REVALIDATE_API_KEY) {
     return Response.json(
