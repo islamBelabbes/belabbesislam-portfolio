1fdd273669f8969b22007c6b1ea35aa41dd0c5e4
islamBelabbes <belabbesislam2@gmail.com>
2024-10-16 21:00:43 +0100

    add dynamic year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 function Footer() {
+  const data = new Date();
   return (
     <footer>
       <div className="lg:px-[80px] py-[24px] flex justify-center bg-WhiteSecondary dark:bg-BlackSecondary w-full text-[14px] lg:text-[18px]">
         <div className="flex gap-3 pr-4 border-r dark:border-WhiteSecondary border-BlackSecondary">
-          © 2023
+          © {data.getFullYear()}
         </div>
         <span className="pl-4">
           <a href=""> Coded </a>
