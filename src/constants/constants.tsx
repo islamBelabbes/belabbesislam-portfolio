0597229c8672dd621ed063500b2c17eafbcdd472
islamBelabbes <belabbesislam2@gmail.com>
2025-04-03 19:43:00 +0100

    add constants

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -71,3 +71,12 @@ export const TOAST_IDs = {
 
 export const categoriesTableDataLimit = 10;
 export const projectsTableDataLimit = 10;
+
+// export const SITE_NAME = "شبكة المعرفة";
+
+export const MEDIA_URL = "https://utfs.io/f";
+
+export const PAGINATION = {
+  LIMIT: 5,
+  PAGE: 1,
+};
