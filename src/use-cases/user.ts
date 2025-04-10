d26e479c72d891833a9867c781e44a8862989650
islamBelabbes <belabbesislam2@gmail.com>
2025-04-10 21:35:53 +0100

    return undefined instead of null

--- src/use-cases/user.ts
+++ src/use-cases/user.ts
@@ -3,5 +3,5 @@ import { currentUser } from "@clerk/nextjs/server";
 
 export const getCurrentUserUseCase = async () => {
   const user = await currentUser();
-  return user ? userDtoMapper(user) : null;
+  return user ? userDtoMapper(user) : undefined;
 };
