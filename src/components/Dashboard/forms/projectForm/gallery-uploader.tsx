eaef94a488716ae75303f047941b53cb18448f5b
islamBelabbes <belabbesislam2@gmail.com>
2025-05-07 23:05:08 +0100

    ui

--- src/components/Dashboard/forms/projectForm/gallery-uploader.tsx
+++ src/components/Dashboard/forms/projectForm/gallery-uploader.tsx
@@ -1,50 +1,64 @@
-import React, { useCallback, useMemo } from "react";
-import { useDropzone } from "react-dropzone";
-import { cn } from "@/lib/utils";
-import { Images } from "lucide-react";
+// import React, { useCallback } from "react";
+// import { useDropzone } from "react-dropzone";
+// import { cn } from "@/lib/utils";
+// import { Images } from "lucide-react";
+// import { nanoid } from "nanoid";
 
-type ImageUploadProps = {
-  gallery: File[];
-  setGallery: (image: File[]) => void;
-  className?: string;
-  disabled?: boolean;
-};
+// export type GalleryItem = {
+//   id: string;
+//   file: File;
+// };
 
-const GalleryUploader = ({
-  setGallery,
-  className,
-  disabled = false,
-}: ImageUploadProps) => {
-  const onDrop = useCallback(
-    (acceptedFiles: File[]) => {
-      setGallery(acceptedFiles);
-    },
-    [setGallery]
-  );
+// type GalleryUploadProps = {
+//   gallery: GalleryItem[];
+//   setGallery: (image: GalleryItem[]) => void;
+//   className?: string;
+//   disabled?: boolean;
+// };
 
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    accept: {
-      "image/png": [".png", ".jpg", ".jpeg"],
-    },
-    multiple: true,
-  });
+// const GalleryUploader = ({
+//   gallery,
+//   setGallery,
+//   className,
+//   disabled = false,
+// }: GalleryUploadProps) => {
+//   const onDrop = useCallback(
+//     (acceptedFiles: File[]) => {
+//       const fields = acceptedFiles.map((file) => ({
+//         id: nanoid(),
+//         file,
+//       }));
+//       return setGallery([...gallery, ...fields]);
+//     },
+//     [setGallery, gallery]
+//   );
 
-  return (
-    <div
-      {...getRootProps({
-        onClick: (event) => disabled && event.stopPropagation(),
-      })}
-      className={cn(
-        "dropzone rounded-md w-[200px] aspect-video cursor-pointer flex items-center justify-center border  text-sm gap-1 bg-gray-950/10",
-        className
-      )}
-    >
-      <input {...getInputProps({ className: "dropzone" })} disabled />
-      <Images />
-      add to gallery
-    </div>
-  );
-};
+//   const { getRootProps, getInputProps } = useDropzone({
+//     onDrop,
+//     accept: {
+//       "image/png": [".png", ".jpg", ".jpeg"],
+//     },
+//     multiple: true,
+//   });
 
-export default GalleryUploader;
+//   return (
+//     <div
+//       {...getRootProps({
+//         onClick: (event) => disabled && event.stopPropagation(),
+//       })}
+//       className={cn(
+//         "dropzone rounded-md w-[200px] aspect-video cursor-pointer flex items-center justify-center border  text-sm gap-1 bg-gray-950/10",
+//         className
+//       )}
+//     >
+//       <input
+//         {...getInputProps({ className: "dropzone" })}
+//         disabled={disabled}
+//       />
+//       <Images />
+//       add to gallery
+//     </div>
+//   );
+// };
+
+// export default GalleryUploader;
